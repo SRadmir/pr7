ff257e096615f0abfae66b52d4eac36325a84bfe
Radmir Sharafutdinov <rsarafutdinov186@icloud.com>
2024-10-16 03:35:52 +0500

    Добавил возможность переводить отрицательные числа в задании 3

--- homework3/zadanie3.py
+++ homework3/zadanie3.py
@@ -5,13 +5,18 @@ def qu(q):
     p = q % 4
     ls.append(p)
     qu(q // 4)
-a = int(input("Введите целое, неорицательное число в десятичной системе счисления "))
+a = int(input("Введите целое число в десятичной системе счисления "))
 if a==0:
         print('0')
-elif a>0:
+if a>0:
     qu(a)
     ls.reverse()
     chislo=int(str(ls)[1:-1].replace(', ',''))
     print(chislo)
 else:
-    print("Неверный ввод")
+    a*=-1
+    qu(a)
+    ls.reverse()
+    chislo=int(str(ls)[1:-1].replace(', ',''))
+    print("математическая запись: -",chislo)
+    
